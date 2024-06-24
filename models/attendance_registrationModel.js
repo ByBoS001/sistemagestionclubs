1843272279c2b0a702c90e50ae547e25294b9d37
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 13:35:06 -0500

    commit

---
+++ models/attendance_registrationModel.js
@@ -0,0 +1,12 @@
+class attendance_registration {
+    constructor(idattendance_registration, attendance, club_activity_has_user_role_club_activity_idclub_activity, club_activity_has_user_role_user_role_iduser_role, club_activity_has_user_role_user_role_club_idclub, club_activity_has_user_role_user_role_user_iduser ) {
+      (this.idattendance_registration = idattendance_registration),
+        (this.attendance = attendance),
+        (this.club_activity_has_user_role_club_activity_idclub_activity = club_activity_has_user_role_club_activity_idclub_activity),
+        (this.club_activity_has_user_role_user_role_iduser_role = club_activity_has_user_role_user_role_iduser_role),
+        (this.club_activity_has_user_role_user_role_club_idclub = club_activity_has_user_role_user_role_club_idclub),
+        (this.club_activity_has_user_role_user_role_user_iduser = club_activity_has_user_role_user_role_user_iduser);
+    }
+  }
+  
+export default attendance_registration;
---
+++ models/user_has_authenticationModel.js
@@ -0,0 +1,8 @@
+class user_has_authentication {
+    constructor(user_iduser, authentication_idauthentication) {
+      (this.user_iduser = user_iduser),
+      (this.authentication_idauthentication = authentication_idauthentication);
+    }
+  }
+  
+export default user_has_authentication;
