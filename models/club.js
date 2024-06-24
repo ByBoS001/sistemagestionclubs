5a95ffb0f0cae6d9680762a925993121aa32ce23
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 13:10:13 -0500

    a

---
+++ models/club.js
@@ -0,0 +1,12 @@
+class club {
+    constructor(idclub, name, description, category_id_category, policy_idpolicy, resource_idresource) {
+      (this.idclub = idclub),
+        (this.name = name),
+        (this.description = description),
+        (this.category_id_category = category_id_category),
+        (this.policy_idpolicy = policy_idpolicy),
+        (this.resource_idresource = resource_idresource);
+    }
+  }
+  
+export default club;
