544f8e68f4625d735a8672fc4da75bbcd107b7a0
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 13:59:53 -0500

    a

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,10 @@
 class User {
-    constructor(idUser, name, email, cellphone, category_id_cateory, campus_idcampus) {
+    constructor(idUser, name, email, cellphone, category_id_category, campus_idcampus) {
       (this.idUser = idUser),
         (this.name = name),
         (this.email = email),
         (this.cellphone = cellphone),
-        (this.category_id_cateory = category_id_cateory),
+        (this.category_id_category = category_id_category),
         (this.campus_idcampus = campus_idcampus);
     }
   }
