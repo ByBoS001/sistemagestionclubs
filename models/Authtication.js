ff5355fd10924da77710acb6cf109aea3bbe3427
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 12:59:32 -0500

    cuarto commit

--- models/Authtication.js
+++ models/Authtication.js
@@ -1,10 +1,9 @@
-class Authentication {
-    constructor(idAuthentication, authentication_method_idauthentication_method) {
-      (this.idAuthentication = idAuthentication),
-        (this.authentication_method_idauthentication_method = authentication_method_idauthentication_method);
-        
+class authentication {
+    constructor(idauthentication, authentication_method_idauthentication_method) {
+      (this.idauthentication = idauthentication),
+        (this.authentication_method_idauthentication_method = authentication_method_idauthentication_method); 
     }
   }
-
-export default Authentication;
+  
+export default authentication;
 
---
+++ models/campus.js
@@ -0,0 +1,9 @@
+class campus {
+    constructor(idcampus, name_campus, university_iduniversity ) {
+      (this.idcampus = idcampus),
+        (this.name_campus = name_campus),
+        (this.university_iduniversity = university_iduniversity);
+    }
+  }
+  
+export default campus;
