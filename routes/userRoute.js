2fb61508a51badbba2794f4c9d8442fe2840d6b9
ByBoS001 <brortegasa@uide.edu.ec>
2024-06-24 13:40:34 -0500

    sexto comit

---
+++ routes/userRoute.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.get('/', getUsers);
+router.post('/newUser', createUser);
+router.get('/user/:id', getUser);
+router.put('/updateUser/:id', updateUser);
+router.delete('/deleteUser/:id', deleteUser);
+
+export default router;
