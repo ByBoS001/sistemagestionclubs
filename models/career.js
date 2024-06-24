417e67934f3b4c6dd7c966dcf06505592b9437c4
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 13:07:42 -0500

    otro commit

---
+++ models/career.js
@@ -0,0 +1,9 @@
+class career {
+    constructor(idcareer, name, description) {
+      (this.idcareer = idcareer),
+        (this.name = name),
+        (this.description = description);
+    }
+  }
+  
+export default career;
