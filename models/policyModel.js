4fb0404be11372438f3e935fc4db01e11d48c781
gariosfe <gariosfe@uide.edu.ec>
2024-06-24 13:16:14 -0500

    septimo committ

---
+++ models/policyModel.js
@@ -0,0 +1,9 @@
+class policy {
+    constructor(idpolicy, description, mandatory) {
+      (this.idpolicy = idpolicy),
+        (this.description = description),
+        (this.mandatory = mandatory);
+    }
+  }
+  
+export default policy;
---
+++ models/resourceModel.js
@@ -0,0 +1,10 @@
+class resource {
+    constructor(idresource, name, description, amount) {
+      (this.idresource = idresource),
+        (this.name = name),
+        (this.description = description),
+        (this.amount = amount);
+    }
+  }
+  
+export default resource;
