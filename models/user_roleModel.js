65042843dd7b2ff29cbe09e6660429900265b946
ByBoS001 <brortegasa@uide.edu.ec>
2024-06-24 15:40:23 -0500

    Update user_roleModel.js

--- models/user_roleModel.js
+++ models/user_roleModel.js
@@ -1,10 +1,11 @@
+import iduser from '../models/userModel.js';
 class user_role {
-    constructor(iduser_role, name_role, club_idclub, user_iduser) {
+    constructor(iduser_role, name_role, club_idclub, iduser) {
       (this.iduser_role = iduser_role),
         (this.naname_roleme = name_role),
         (this.club_idclub = club_idclub),
-        (this.user_iduser = user_iduser);
+        (this.user_iduser = iduser);
     }
   }
   
-export default user_role;
+export default user_role;   
