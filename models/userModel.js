519ba24c4c376124e3fa34e5c3ef09dcccc120f0
ByBoS001 <brortegasa@uide.edu.ec>
2024-06-25 16:22:34 -0500

    ultimo

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,13 @@
+
 class user {
-    constructor(iduser, name, email, cellphone, category_id_category, campus_idcampus) {
+    constructor(iduser, name, email, cellphone, category_id_category, campus_idcampus, name_role) {
       (this.iduser = iduser),
         (this.name = name),
         (this.email = email),
         (this.cellphone = cellphone),
         (this.category_id_category = category_id_category),
-        (this.campus_idcampus = campus_idcampus);
+        (this.campus_idcampus = campus_idcampus),
+        (this.role_name=name_role)
     }
   }
 
--- models/user_roleModel.js
+++ models/user_roleModel.js
@@ -1,10 +1,8 @@
-import iduser from '../models/userModel.js';
 class user_role {
-    constructor(iduser_role, name_role, club_idclub, iduser) {
+    constructor(iduser_role, name_role, club_idclub) {
       (this.iduser_role = iduser_role),
         (this.naname_roleme = name_role),
-        (this.club_idclub = club_idclub),
-        (this.user_iduser = iduser);
+        (this.club_idclub = club_idclub)
     }
   }
   
