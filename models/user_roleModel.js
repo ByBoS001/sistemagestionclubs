f27f2b0f102747b89196b0e5fa03d16e2fe3ef79
ByBoS001 <brortegasa@uide.edu.ec>
2024-06-26 08:33:42 -0500

    ultimo cmoint

--- models/user_roleModel.js
+++
@@ -1,9 +0,0 @@
-class user_role {
-    constructor(iduser_role, name_role, club_idclub) {
-      (this.iduser_role = iduser_role),
-        (this.naname_roleme = name_role),
-        (this.club_idclub = club_idclub)
-    }
-  }
-  
-export default user_role;   
